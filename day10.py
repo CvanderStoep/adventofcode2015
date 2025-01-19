a9c1c0bb0604e404c498614775e09bf3e1991eb3
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2025-01-19 13:14:10 +0100

    day 10 completed

--- day10.py
+++ day10.py
@@ -1,3 +1,6 @@
+import time
+
+
 def read_input_file(file_name: str) -> list:
     with open(file_name) as f:
         content = f.read().splitlines()
@@ -20,12 +23,31 @@ def process_sequence(sequence: str) -> str:
     return new_sequence
 
 
+def process_sequence2(sequence: str) -> str:
+    position = 0
+    new_sequence = []
+    l = len(sequence)
+    while position < l:
+        count = 1
+        while (position + count) < l and sequence[position] == sequence[position + count]:
+            count += 1
+        new_sequence.append(str(count))
+        new_sequence.append(sequence[position])
+        position += count
+    new_sequence = ''.join(new_sequence)
+
+    return new_sequence
+
+
 def compute_part(file_name: str, number_of_sequences=40) -> int:
     sequence = read_input_file(file_name)
     print(sequence)
+    start_time = time.time()
     for _ in range(number_of_sequences):
         sequence = process_sequence(sequence)
+        # sequence = process_sequence2(sequence)
     print(f'{len(sequence)= }')
+    print("Elapsed time:", time.time() - start_time)
 
     return len(sequence)
 
--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import time
 # Using `+=`
 start_time = time.time()
 string = ""
-for i in range(10000):
+for i in range(100000):
     string += "newstring"
 print("Using +=:", time.time() - start_time)
 
+# Using `+`
+start_time = time.time()
+string = ""
+for i in range(100000):
+    string = string + "newstring"
+print("Using +:", time.time() - start_time)
+
 # Using `.join()`
 start_time = time.time()
-string_list = ["newstring" for i in range(10000)]
+string_list = []
+for _ in range(100000):
+    string_list.append("newstring")
 string = "".join(string_list)
 print("Using join:", time.time() - start_time)
